4c02844c9460e143628ff147ed17515b3a77a6eb
pierocarrion <piero.january15@gmail.com>
2023-01-20 22:26:00 -0500

    Final project fixed

--- index.js
+++ index.js
@@ -1,49 +1,64 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bp = require('body-parser')
+
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
 
 const originalUrls = []
-const shortUrls = []
-
 
 app.post('/api/shorturl', (req, res) => {
-  const url = req.body.url
-  const foundIndex = originals.indexOf(url)
-  if (!url.includes("https://") || !url.includes("http://")) {
-    return res.json({ error: "invalid url" })
-  }
+  try {
+    const url = req.body.url
+    const foundIndex = originalUrls.indexOf(url)
 
-  if (foundIndex < 0) {
-    originalUrls.push(url)
-    shortUrls.push(shortUrls.length)
+    if (!url.includes("https://") && !url.includes("http://")) {
+      return res.json({ error: "invalid url" })
+    }
+
+    if (foundIndex < 0) {
+      originalUrls.push(url)
+      return res.json({
+        original_url: url,
+        short_url: originalUrls.length - 1
+      })
+    }
     return res.json({
       original_url: url,
-      short_url: shortUrls.length - 1
+      short_url: foundIndex
     })
+  } catch (error) {
+    console.log('🚀 ~ file: index.js:34 ~ app.post ~ error', error)
   }
-  return res.json({
-    original_url: url,
-    short_url: shortUrls[foundIndex]
-  })
+})
+
+app.get('/api/shorturl/:short', (req, res) => {
+    const shortUrl = req.params.short
+    console.log('🚀 ~ file: index.js:62 ~ app.get ~ shortUrl =', shortUrl)
+    console.log('🚀 ~ file: index.js:64 ~ app.get ~ shortUrl', originalUrls)
+    const originalUrl = originalUrls[shortUrl] 
+    console.log(originalUrl)
+    return res.redirect(originalUrl)
 })
